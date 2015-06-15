eead4ee491c7ece494c671976071d9884849b1d9
root <root@pittula.freepto.mx>
2015-06-15 11:25:17 +0200

    base info

---
+++ roles/build/files/usr/local/bin/manualbuild.sh
@@ -0,0 +1,175 @@
+#!/usr/bin/env bash
+### given a directory, it will build the image inside, then put it under $WWW
+
+set -e
+set -u
+
+VARIANT=
+# if release is "1", different naming will be used
+RELEASE=${RELEASE:-0}
+if [ -n "${LB_ADDITIONAL_CONF}" ] && [ -r "${LB_ADDITIONAL_CONF}" ]; then
+	echo "Reading additional configuration files..." >&2
+	source "${LB_ADDITIONAL_CONF}"
+fi
+
+while getopts 'v:' opt; do
+	case $opt in
+		v)
+			VARIANT="-${OPTARG}"
+			;;
+		\?)
+			exit 2
+			;;
+	esac
+done
+
+WWW=/var/www/dev
+BASEDIR=${1%/} #strip slash
+DATEFMT='%y%m%d_%H.%M'
+if [ -r /etc/http_proxy ]; then
+	export http_proxy="$(cat /etc/http_proxy)"
+else
+	export http_proxy="http://localhost:3142/"
+fi
+echo "vado di proxy $http_proxy"
+HEAD=""
+imgname=""
+
+# CHECK DISK SPACE
+DISKLIMIT=80
+diskusage=`df -H "$WWW"| tail -n1 | awk '{ print $5 }' | cut -d'%' -f1`
+if [ "$diskusage" -gt "$DISKLIMIT" ]; then
+        echo "[ABORT] There isn't enough disk space."
+	echo "Disk usage: ${diskusage}% - Disk limit: ${DISKLIMIT}%"
+	exit 1
+fi
+
+
+if [[ $# -lt 1 ]]; then
+	echo "you must fill config\n $0 path locale timezone keymap"
+	exit 1
+fi
+if ! [[ -d $1/.git ]]; then
+	echo "repository does not exist, create it manually"
+	exit 1
+fi
+cd $BASEDIR
+
+test -d $BASEDIR || exit 1
+
+export GIT_WORK_TREE=$BASEDIR
+export GIT_DIR=$GIT_WORK_TREE/.git
+
+HEAD=$(git rev-parse HEAD | head -c 6)
+builddate=$(date +$DATEFMT)
+if [[ "$RELEASE" -eq 1 ]]; then
+	imgdesc="freepto-$(git describe)$VARIANT"
+	parentdir="freepto-$(git describe)"
+else
+	imgdesc="${builddate}_$(git describe --tags --long)$VARIANT"
+	parentdir=$(basename $BASEDIR)
+fi
+imgname="${WWW%/}/${parentdir}/${imgdesc}/${imgdesc}.img"
+mkdir -p "$(dirname $imgname)"
+echo "Target name will be $imgname"
+
+if [[ -n "$(find ${WWW%/}/ -name "*_$HEAD-$VARIANT.img" -print -quit)" ]]; then
+	echo "an image for $HEAD already exists: "
+	ls -l ${WWW%/}/*_$HEAD-$VARIANT.img
+	exit 1
+else
+	echo "ok, building $HEAD"
+fi
+
+set -e
+
+mkdir -p /var/tmp/build
+avail_mega=$(df -BM /var/tmp/build | awk '{ print $4 }' | tail -n1 | sed 's/[^0-9]//g')
+if [ "$avail_mega" -lt 5000 ]; then
+	echo "Too few disk space: $avail_mega MB, aborting" >&2
+	exit 5
+fi
+workdir=$(mktemp -d /var/tmp/build/build_$(basename $BASEDIR).XXXXX)
+rsync -r --links $BASEDIR/ $workdir/
+
+cleanup()
+{
+	troubledir="/var/build/trouble"
+	mkdir -p "$troubledir"
+	if [ -d "$workdir" ]
+	then
+		echo "Something unexpected happened; you can find everything in $troubledir "
+		mount | fgrep "$workdir" | fgrep /chroot/|awk '{ print $3 }'| \
+		while read d
+		do
+			umount $d
+		done
+
+		mv $workdir "$troubledir"
+		echo "Debug build in $troubledir/$(basename $workdir)"
+	fi
+}
+trap cleanup EXIT
+
+cd $workdir/
+echo -n "sono in "
+pwd
+
+lb clean
+
+# Info files known in advance
+git log -n 20 --decorate=short --stat > ${imgname}.history.txt
+cp config/freepto ${imgname}.config.txt
+log="${imgname}.log.txt"
+
+mkdir -p /var/log/build
+
+if ! lb config 2>&1 | tee -a "$log" ; then
+	echo "errori nel config"
+	cd ~
+	exit 3
+fi
+
+[[ -r config/freepto.local ]] && cat config/freepto.local >> ${imgname}.config.txt
+
+if ! lb build 2>&1 | tee -a "$log" ; then
+	echo "errori nel build"
+	cd ~
+	exit 3
+fi
+
+if ! [ -f binary.img ];
+then
+	echo "binary not found"
+	cd ~
+	exit 4
+fi
+
+
+### "Extra" files
+mv binary.contents ${imgname}.contents.txt
+mv chroot.packages.live ${imgname}.packages.txt
+if [[ -f pkgs.log ]]; then
+	mv pkgs.log ${imgname}.pkgs_log.txt
+fi
+
+### Image itself
+mv binary.img $imgname
+if ! vboxmanage convertdd  ${imgname} ${imgname%.img}.vdi; then
+    echo "vboxmanage error $?"
+fi
+if [ -f ${imgname%.img}.vdi ]; then
+    chmod a+r ${imgname%.img}.vdi
+fi
+cd $(dirname ${imgname})
+sha512sum $(basename ${imgname}) > ${imgname}.sha512sum.txt
+sha512sum $(basename ${imgname%.img}.vdi) > ${imgname%.img}.vdi.sha512sum.txt
+
+cd ~
+rm -rf $workdir
+
+# remove old image on /var/www and /var/tmp/build/build*
+find $WWW/ -type f -name "$(basename $BASEDIR)-*" -mtime +3 -delete
+find /var/tmp/build/git* -type f -mtime +1 -delete
+
+# vim: set ft=sh:
